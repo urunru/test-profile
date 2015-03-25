1e45bb02d8f2e29cee338bff8baccb1c697001c7
村上優孝 <murakami@TgMacPro04-no-MacBook-Pro.local>
2015-03-25 16:56:28 +0900

    Initial Commit

---
+++ testprofile/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testprofile
+//
+//  Created by 村上優孝 on 2015/03/25.
+//  Copyright (c) 2015年 村上優孝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testprofile/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testprofile
+//
+//  Created by 村上優孝 on 2015/03/25.
+//  Copyright (c) 2015年 村上優孝. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
